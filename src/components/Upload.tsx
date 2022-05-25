a7a3611e6c2b750a576ac81f555f03704c8959ba
Cho-ui <christian.lindell@myy.haaga-helia.fi>
2022-05-25 16:22:13 +0300

    Lock-file parsing to console implemented

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
@@ -8,15 +8,36 @@ const Input = styled('input')({
 });
 
 export default function Upload() {
+    const [selectedFile, setSelectedFile] = useState<File>();
+
+    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const files = event.target.files;
+        if (!files) return;
+
+        console.log(files[0]);
+        setSelectedFile(files[0]);
+    }
+
+    const parseFile = async () => {
+        try {
+            const fileText = await selectedFile?.text();
+            console.log(fileText);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
     return (
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: 100 }}>
             <label htmlFor="contained-button-file">
-                <Input accept=".lock" id="contained-button-file" multiple type="file" />
+                <Input accept=".lock" id="contained-button-file"
+                    multiple={false} onChange={handleFileChange} type="file" />
                 <Stack direction="row" spacing={3}>
                     <Button variant="contained" component="span">
                         Upload Poetry.lock file
                     </Button>
-                    <Button variant="contained">
+                    <Button variant="contained" onClick={() => parseFile()}>
                         Parse
                     </Button>
                 </Stack>
